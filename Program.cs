383473fbaa73941cddb34cf7402ae3f187e1c924
ed.lopez1905@gmail.com <ed.lopez1905@gmail.com>
2018-10-17 18:58:56 -0700

    upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a simpel while loop:
+            //int i = 1;
+
+            //while (i <= 5)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+
+            //}
+
+            // a for loop can do basically everything a while can do and its cleaner to code in(sometimes)
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+            //Console.ReadLine();
+
+
+
+            //with an array
+            //int [] luckNumbers = {4, 8, 15, 16, 23, 42}
+            //for (int i = 0; i < luckNumbers.Length; i++)
+            //{
+            //    Console.WriteLine(luckNumbers[i]);
+            //}
+
+
+
+            //Console.ReadLine();
+
+
+
+        }
+    }
+}
